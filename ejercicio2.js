f81a10562be41e6b385362ab3cbf3d4385280591
Kaela <akoseguera@unah.hn>
2025-02-09 23:54:56 -0600

    Agregado del ejercicio2 completo

---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+//Ejercicio 2
+//Actualizar stock por ID
+function actualizarStock(productos){
+
+}
+
+const productos = [
+    { id: 1, nombre: "Lápiz", stock: 10 },
+    { id: 2, nombre: "Cuaderno", stock: 5 },
+];
