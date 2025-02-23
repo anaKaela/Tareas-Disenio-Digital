11263e64a5ff6bc55514f20db43558fea4f412c3
Kaela <akoseguera@unah.hn>
2025-02-22 22:01:58 -0600

    Agregado index.html

--- tarea1.1/ejercicio1.js
+++ tarea1.1/ejercicio1.js
@@ -1,17 +1,16 @@
 //Ejercicio 1
 //Filtrar y transformar productos
 function aplicarImpuestos(productos) {
-    return productos
-        .filter(producto => producto.precio > 50) // Filtra productos con precio mayor a 50
-        .map(producto => ({ 
-            ...producto, 
-            precio: producto.precio * 1.10 // Aplica el 10% de impuesto
-        }));
+  return productos
+    .filter((producto) => producto.precio > 50) // Filtra productos con precio mayor a 50
+    .map((producto) => ({
+      ...producto,
+      precio: producto.precio * 1.1, // Aplica el 10% de impuesto
+    }));
 }
 const productos = [
-    { nombre: "Camisa", precio: 45 },
-    { nombre: "Zapatos", precio: 80 },
+  { nombre: "Camisa", precio: 45 },
+  { nombre: "Zapatos", precio: 80 },
 ];
 const productosConImpuesto = aplicarImpuestos(productos);
 console.log(productosConImpuesto);
-
